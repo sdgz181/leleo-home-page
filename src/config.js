167a4b27a0a5d964a4b2b0c84adaa10704ac7549
sdgz181 <sdgz181@gmail.com>
2025-07-29 11:14:44 +0800

    更新 config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
 	//网页元数据
 	metaData: {
-		title: 'Leleo的个人主页🎉',
-		description: '欢迎来到Leleo的奇妙世界！',
-		keywords: 'Leleo,leleo,个人主页,个人网站',
+		title: 'From the 𝙈𝙤𝙤𝙣 的个人主页🎉',
+		description: '欢迎来到𝓶𝓸𝓸𝓷的奇妙世界！',
+		keywords: 'From the 𝙈𝙤𝙤𝙣,𝓶𝓸𝓸𝓷,个人主页,个人网站',
 		icon: '/favicon.ico'   //网页图标，支持外链
 	},
 
